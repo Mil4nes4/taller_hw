c99554b77acec09c2ed0987a54313f2f8de0d578
Larry <larryjpa2006@gmail.com>
2025-04-26 21:47:14 -0500

    wa2

--- reto5.cpp
+++ reto5.cpp
@@ -1,53 +1,44 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
-    int n;    
-        cout << "Escriba un numero n impar (n>=5): ";
-        cin >> n; 
-    if (n >= 5 && n % 2 == 1){
-    	int i, j;    
-    	for(i = 1; i <= (n+1)/2; i++) {
+    int n;
+    cout << "Ingrese un numero n impar (n>=5): ";
+    cin >> n;
+    if (n >= 5 && n%2 == 1) {
+	    for (int i = 0; i < n; ++i) {
+	        cout << "* ";
+	    }
+	    cout << endl;
+	    int m = n - 2;
+	    for (int i = 0; i < m; ++i) {
+	        int esp1 = i + 1;
+	        int esp2 = m - i - 1;
+	        int esp3 = i * 2;
+	        for (int i = 0; i < esp1; ++i) {
+	            cout << " ";
+	        }
+    	    cout << "* ";
+    	    for (int i = 0; i < esp2; ++i) {
+    	        cout << "  ";
+    	    }
     	    cout << "*";
-    	}
-    	cout << endl;
-    	for(j = 1; j <= (n+1)/2 - 2; j++) {
-        	for(i = 1; i <= (n+1)/2; i++) {
-            	if(i > 1 && i < ((n+1)/2)) {
-                	cout << " ";
-            	} else {
-                	cout << "*";
-            	}
+    	    for (int i = 0; i < esp3; ++i) {
+    	        cout << " ";
         	}
+        	cout << " *";
         	cout << endl;
     	}
-    	for(i = 1; i <= n; i++) {
-        	cout << "*";
+    	for (int i = 0; i < n - 1; ++i) {
+    	    cout << " ";
+    	}
+    	for (int i = 0; i < n; ++i) {
+        	cout << "* ";
     	}
     	cout << endl;
-    	for(j = 1; j <= (n+1)/2 - 2; j++) {
-        	for(i = 1; i <= n; i++) {
-            	if(i >= 1 && i < (n+1)/2) {
-                	cout << " ";
-            	} else {
-                	if(i > (n+1)/2 && i < n) {
-                    	cout << " ";
-                	} else {
-                    	cout << "*";
-                	}
-            	}
-        	}
-        	cout << endl;
-    	}
-    	for(i = 1; i <= n; i++) {
-        	if((i >= 1) && (i < (n+1)/2)) {
-            	cout << " ";
-        	} else {
-            	cout << "*";
-        	}
     }
-	}
-	else {
-		cout<<"El numero es invalido";
+    else {
+    	cout<<"El numero es invalido ";
 	}
     return 0;
 }
